49aea8f5fd304400ee3eb9c59fca5f9408671a6c
Weerapat <wond3rschool@gmail.com>
2017-10-16 05:06:46 +0700

    init git

--- src/pages/modals-song/modals-song.ts
+++ src/pages/modals-song/modals-song.ts
@@ -15,16 +15,10 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ModalsSongPage {
 
 status:boolean;
-statusButton:boolean;
-statusRepeat:boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  shuffle() {
-    this.status = !this.status;
-  }
-  repeat(){
-    this.status = !this.status;
-  }
+
   changeButton() {
     this.status = !this.status;
   }
@@ -32,9 +26,7 @@ statusRepeat:boolean;
   closeTapped(){
     this.navCtrl.pop();
   }
-
   
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalsSongPage');
   }
